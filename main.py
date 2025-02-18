1e44d055ed0bc409df43f8b0abebae85c9539250
HARRY GUEVARA <123556401+HarryGuevara@users.noreply.github.com>
2025-02-18 13:34:05 -0500

    Update main.py
    puerto modificado

--- main.py
+++ main.py
@@ -240,6 +240,7 @@ import os
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
-
+    port = int(os.environ.get("PORT", 10000))
+    print(f"Iniciando en puerto: {port}")
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
